f823036f9001a2cf6121a39bdd881bd6410694a4
Utibe-ima Samuel <utysamuel@gmail.com>
2022-06-30 21:09:15 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - this command prints the exact script 'Programming is like building a multilingual puzzle, followed by a new line'
+ * Return: Always (0) 
+ * 
+ * */
+
+int main (void){
+
+	puts("Programming is like building a multilingual puzzle, followed by a new line")
+}
